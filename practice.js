71911f159eabf93d6ceab08598b6f18624c562ab
MAXHEAD3252 <akshat3252@gmail.com>
2023-10-17 14:49:35 +0530

    files updated

---
+++ practice.js
@@ -0,0 +1,20 @@
+function ispallindrome(string){
+    // convert string to an array
+   const arrayValues = string.split('');
+
+   // reverse the array values
+   const reverseArrayValues = arrayValues.reverse();
+
+   // convert array to string
+   const reverseString = reverseArrayValues.join('');
+
+   if(string == reverseString) {
+       return true
+   }
+   else {
+       return false
+   }
+   
+}
+
+console.log(ispallindrome('racecar'))
